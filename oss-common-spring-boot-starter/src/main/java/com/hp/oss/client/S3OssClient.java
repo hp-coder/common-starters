759bec457865b43cb7742640336cc8196ca88104
胡鹏 <hup@lubanjianye.com>
2022-08-25 17:07:07 +0800

    OSS based on AmazonS3: common starter

---
+++ oss-common-spring-boot-starter/src/main/java/com/hp/oss/client/S3OssClient.java
@@ -0,0 +1,63 @@
+package com.hp.oss.client;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import com.amazonaws.services.s3.model.S3Object;
+import lombok.RequiredArgsConstructor;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+/**
+ * 亚马逊s3协议客户端
+ *
+ * @author HP
+ * @date 2022/8/25
+ */
+@RequiredArgsConstructor
+public class S3OssClient implements OssClient {
+
+    private final AmazonS3 amazonS3;
+
+    @Override
+    public void createBucket(String bucketName) {
+        if (amazonS3.doesBucketExistV2(bucketName)) {
+            return;
+        }
+        amazonS3.createBucket(bucketName);
+    }
+
+    @Override
+    public void bucketPolicy(String bucketName,String policy){
+        amazonS3.setBucketPolicy(bucketName, policy);
+    }
+
+    @Override
+    public String getObjectURL(String bucketName, String objectName) {
+        final URL url = amazonS3.getUrl(bucketName, objectName);
+        return url.toString();
+    }
+
+    @Override
+    public S3Object getObject(String bucketName, String objectName) {
+        return amazonS3.getObject(bucketName, objectName);
+    }
+
+    @Override
+    public PutObjectResult putObject(String bucketName, String objectName, InputStream inputStream, long size, String contentType) throws IOException {
+        ObjectMetadata metaData = new ObjectMetadata();
+        metaData.setContentLength(size);
+        metaData.setContentType(contentType);
+        final PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName, inputStream, metaData);
+        putObjectRequest.getRequestClientOptions().setReadLimit((int) (size + 1));
+        return amazonS3.putObject(putObjectRequest);
+    }
+
+    @Override
+    public AmazonS3 getS3Client() {
+        return amazonS3;
+    }
+}
---
+++ oss-common-spring-boot-starter/src/main/java/com/hp/oss/config/OssProperties.java
@@ -0,0 +1,33 @@
+package com.hp.oss.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * OSS配置信息
+ *
+ * @author HP
+ * @date 2022/8/25
+ */
+@ConfigurationProperties(prefix = "oss")
+@Setter
+@Getter
+public class OssProperties {
+
+    private boolean enable = true;
+    private String accessKey;
+    private String accessSecret;
+    /**
+     * 如果是服务器MinIO等直接使用 [$schema]://[$ip]:[$port]
+     * 外网[$Schema]://[$Bucket].[$Endpoint]/[$Object]*
+     * https://help.aliyun.com/document_detail/375241.html*
+     */
+    private String endpoint;
+    /**
+     * refres to com.amazonaws.regions.Regions*
+     * https://help.aliyun.com/document_detail/31837.htm?spm=a2c4g.11186623.0.0.695178eb0nD6jp*
+     */
+    private String region;
+    private boolean pathStyleAccess = true;
+}
