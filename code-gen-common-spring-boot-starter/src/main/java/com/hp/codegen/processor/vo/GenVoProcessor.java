22fa6d24ba0a84060293af15fbf54e3c2f056a4e
胡鹏 <hup@lubanjianye.com>
2023-01-24 16:43:41 +0800

    修改VO和DTO的生成位置，做编译后自动化更新

--- code-gen-common-spring-boot-starter/src/main/java/com/hp/codegen/processor/vo/GenVoProcessor.java
+++ code-gen-common-spring-boot-starter/src/main/java/com/hp/codegen/processor/vo/GenVoProcessor.java
@@ -2,7 +2,6 @@ package com.hp.codegen.processor.vo;
 
 import com.google.auto.service.AutoService;
 import com.hp.codegen.processor.AbstractCodeGenProcessor;
-import com.hp.codegen.processor.response.GenResponse;
 import com.hp.codegen.spi.CodeGenProcessor;
 import com.luban.common.jpa.vo.AbstractBaseJpaVO;
 import com.squareup.javapoet.MethodSpec;
@@ -31,21 +30,15 @@ public class GenVoProcessor extends AbstractCodeGenProcessor {
     protected void generateClass(TypeElement typeElement, RoundEnvironment roundEnvironment) {
         Set<VariableElement> fields = findFields(typeElement, v -> Objects.isNull(v.getAnnotation(Ignore.class)));
         String sourceClassName = typeElement.getSimpleName() + SUFFIX;
-        TypeSpec.Builder builder = TypeSpec.classBuilder(sourceClassName)
-                .superclass(AbstractBaseJpaVO.class)
-                .addModifiers(Modifier.PUBLIC)
-                .addAnnotation(Data.class);
+        TypeSpec.Builder builder = TypeSpec.classBuilder(sourceClassName).superclass(AbstractBaseJpaVO.class).addModifiers(Modifier.PUBLIC).addAnnotation(Data.class);
         generateGettersAndSettersWithLombok(builder, fields);
-        MethodSpec.Builder constructorSpecBuilder = MethodSpec.constructorBuilder()
-                .addParameter(TypeName.get(typeElement.asType()), "source")
-                .addModifiers(Modifier.PUBLIC);
+        MethodSpec.Builder constructorSpecBuilder = MethodSpec.constructorBuilder().addParameter(TypeName.get(typeElement.asType()), "source").addModifiers(Modifier.PUBLIC);
         constructorSpecBuilder.addStatement("super(source)");
         fields.forEach(f -> constructorSpecBuilder.addStatement("this.set$L(source.get$L())", getFieldMethodName(f), getFieldMethodName(f)));
-        builder.addMethod(MethodSpec.constructorBuilder()
-                .addModifiers(Modifier.PROTECTED)
-                .build());
+        builder.addMethod(MethodSpec.constructorBuilder().addModifiers(Modifier.PROTECTED).build());
         builder.addMethod(constructorSpecBuilder.build());
-        generateJavaSourceFile(generatePackage(typeElement),typeElement.getAnnotation(GenResponse.class).sourcePath(), builder);
+//        generateJavaSourceFile(generatePackage(typeElement),typeElement.getAnnotation(GenResponse.class).sourcePath(), builder);
+        generateJavaFile(generatePackage(typeElement), builder);
     }
 
     @Override
