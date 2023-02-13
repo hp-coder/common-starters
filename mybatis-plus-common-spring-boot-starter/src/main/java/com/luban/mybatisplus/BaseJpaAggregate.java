561cdf37fc64ab9228ea3bc40d7b2e5262142d97
胡鹏 <hup_dev@outlook.com>
2023-02-13 14:40:46 +0800

    类似jpa的方式增加一个"聚合根"的类

---
+++ mybatis-plus-common-spring-boot-starter/src/main/java/com/luban/mybatisplus/BaseJpaAggregate.java
@@ -0,0 +1,35 @@
+package com.luban.mybatisplus;
+
+import com.baomidou.mybatisplus.annotation.*;
+import com.luban.mybatisplus.convertor.InstantLongConverter;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Data
+public abstract class BaseJpaAggregate {
+
+    @TableId(value = "id", type = IdType.ASSIGN_ID)
+    private Long id;
+
+    @TableField(value = "created_at",
+            updateStrategy = FieldStrategy.IGNORED,
+            typeHandler = InstantLongConverter.class
+    )
+    @Setter(AccessLevel.PROTECTED)
+    private Instant createdAt;
+
+    @TableField(value = "updated_at",
+            typeHandler = InstantLongConverter.class
+    )
+    @Setter(AccessLevel.PROTECTED)
+    private Instant updatedAt;
+
+    @Version
+    @TableField(value = "version")
+    @Setter(AccessLevel.PRIVATE)
+    private Integer version;
+
+}
