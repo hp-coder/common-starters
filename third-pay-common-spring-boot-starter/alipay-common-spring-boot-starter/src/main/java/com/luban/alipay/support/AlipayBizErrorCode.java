e33460b8cf0bf456662d5714f843ba21a6c489c5
胡鹏 <hup_dev@outlook.com>
2023-02-16 11:26:51 +0800

    扩展枚举父类接口的code类型

--- third-pay-common-spring-boot-starter/alipay-common-spring-boot-starter/src/main/java/com/luban/alipay/support/AlipayBizErrorCode.java
+++ third-pay-common-spring-boot-starter/alipay-common-spring-boot-starter/src/main/java/com/luban/alipay/support/AlipayBizErrorCode.java
@@ -1,5 +1,6 @@
 package com.luban.alipay.support;
 
+import com.luban.common.base.enums.BaseEnum;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -10,7 +11,7 @@ import java.util.Optional;
  */
 @Getter
 @AllArgsConstructor
-public enum AlipayBizErrorCode implements BaseEnum<AlipayBizErrorCode> {
+public enum AlipayBizErrorCode implements BaseEnum<AlipayBizErrorCode,String> {
     SYSTEM_ERROR("ACQ.SYSTEM_ERROR", "系统错误:重新发起请求"),
     INVALID_PARAMETER("ACQ.INVALID_PARAMETER", "参数无效:检查请求参数，修改后重新发起请求"),
     TRADE_NOT_EXIST("ACQ.TRADE_NOT_EXIST", "查询的交易不存在:检查传入的交易号是否正确，修改后重新发起请求"),
