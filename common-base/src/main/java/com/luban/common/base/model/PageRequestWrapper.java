402b62ff9379982aa77d4614804412805f2991fa
hupeng <hup@lubanjianye.com>
2023-03-27 12:25:44 +0800

    pagination optimized
    - Optimized the design of the common page request model
    - Strict column-order type
    - mybatis-plus column-order type support
    
    Signed-off-by: hupeng <hup_dev@outlook.com>

--- common-base/src/main/java/com/luban/common/base/model/PageRequestWrapper.java
+++ common-base/src/main/java/com/luban/common/base/model/PageRequestWrapper.java
@@ -1,15 +1,43 @@
 package com.luban.common.base.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 
-import java.util.Map;
+import java.util.List;
 
 
+/**
+ * @author hp
+ */
 @Data
 public class PageRequestWrapper<T> {
 
-  private T bean;
-  private Integer pageSize;
-  private Integer page;
-  private Map<String, String> sorts;
+    private T bean;
+    private Integer pageSize;
+    private Integer page;
+    private List<OrderColumn> sorts;
+
+    @AllArgsConstructor
+    @Getter
+    public static class OrderColumn {
+        private final String columnName;
+        private Order sortedOrder;
+    }
+
+    public static OrderColumn desc(String columnName) {
+        return new OrderColumn(columnName, Order.desc);
+    }
+
+    public static OrderColumn asc(String columnName) {
+        return new OrderColumn(columnName, Order.asc);
+    }
+
+    public enum Order {
+        /**
+         * 排序
+         */
+        asc,
+        desc,
+    }
 }
---
+++ mybatis-plus-common-spring-boot-starter/src/main/java/com/luban/mybatisplus/PageOrderHelper.java
@@ -0,0 +1,30 @@
+package com.luban.mybatisplus;
+
+import com.baomidou.mybatisplus.core.metadata.OrderItem;
+import com.luban.common.base.model.PageRequestWrapper;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * @author hp 2023/3/27
+ */
+public final class PageOrderHelper {
+
+    public static <T> List<OrderItem> orders(PageRequestWrapper<T> wrapper) {
+        final List<PageRequestWrapper.OrderColumn> sorts = wrapper.getSorts();
+        if (CollectionUtils.isEmpty(sorts)) {
+            return Collections.emptyList();
+        }
+        return sorts.stream().map(i -> {
+            if (Objects.equals(i.getSortedOrder(), PageRequestWrapper.Order.desc)) {
+                return new OrderItem(i.getColumnName(), false);
+            } else {
+                return new OrderItem(i.getColumnName(), true);
+            }
+        }).collect(Collectors.toList());
+    }
+}
