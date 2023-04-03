21564d5f843fa086d045a6d9b2d079a88da51495
hupeng <hup_dev@outlook.com>
2023-04-03 16:52:50 +0800

    Converts updated
    Added converts for the type of LocalDate and LocalTime.
    
    Signed-off-by: hupeng <hup_dev@outlook.com>

---
+++ mybatis-plus-common-spring-boot-starter/src/main/java/com/luban/mybatisplus/convertor/LocalDateTypeConverter.java
@@ -0,0 +1,41 @@
+package com.luban.mybatisplus.convertor;
+
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.TypeHandler;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+
+/**
+ * @author hp 2023/3/31
+ */
+public class LocalDateTypeConverter implements TypeHandler<LocalDate> {
+
+    @Override
+    public void setParameter(PreparedStatement ps, int i, LocalDate parameter, JdbcType jdbcType) throws SQLException {
+        ps.setString(i, parameter.format(DateTimeFormatter.ISO_DATE));
+    }
+
+    @Override
+    public LocalDate getResult(ResultSet rs, String columnName) throws SQLException {
+        final String data = rs.getString(columnName);
+        return Optional.ofNullable(data).map(i -> LocalDate.parse(i, DateTimeFormatter.ISO_DATE)).orElse(null);
+    }
+
+    @Override
+    public LocalDate getResult(ResultSet rs, int columnIndex) throws SQLException {
+        final String data = rs.getString(columnIndex);
+        return Optional.ofNullable(data).map(i -> LocalDate.parse(i, DateTimeFormatter.ISO_DATE)).orElse(null);
+    }
+
+    @Override
+    public LocalDate getResult(CallableStatement cs, int columnIndex) throws SQLException {
+        final String data = cs.getString(columnIndex);
+        return Optional.ofNullable(data).map(i -> LocalDate.parse(i, DateTimeFormatter.ISO_DATE)).orElse(null);
+    }
+}
