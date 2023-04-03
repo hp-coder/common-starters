73b1ccedbca986360b4e2ce33004d74c47f34803
hupeng <hup_dev@outlook.com>
2023-04-03 16:32:44 +0800

    package changed
    Signed-off-by: hupeng <hup_dev@outlook.com>

--- extension-common-spring-boot-starter/src/main/java/com/luban/extension/ExtensionAutoConfiguration.java
+++ extension-common-spring-boot-starter/src/main/java/com/luban/extension/ExtensionAutoConfiguration.java
@@ -1,4 +1,4 @@
-package com.luban.extension.config;
+package com.luban.extension;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
