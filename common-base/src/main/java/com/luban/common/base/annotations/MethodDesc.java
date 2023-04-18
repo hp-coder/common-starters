cc272be0c9798d10d58122fd5ded6c24d3650bba
hupeng <hup_dev@outlook.com>
2023-04-18 16:29:20 +0800

    Added an annotation purely for method description usage.

---
+++ common-base/src/main/java/com/luban/common/base/annotations/MethodDesc.java
@@ -0,0 +1,14 @@
+
+package com.luban.common.base.annotations;
+
+import java.lang.annotation.*;
+
+/**
+ * @author HP 2023/2/13
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Documented
+public @interface MethodDesc {
+    String value();
+}
