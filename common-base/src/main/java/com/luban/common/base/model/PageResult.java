dc918329bba56cf7c2ca80d04d92574502cbd6b5
hupeng <hup_dev@outlook.com>
2023-06-09 15:33:47 +0800

    page result model
    Signed-off-by: hupeng <hup_dev@outlook.com>

---
+++ common-base/src/main/java/com/luban/common/base/model/PageResult.java
@@ -0,0 +1,32 @@
+package com.luban.common.base.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+/**
+ * @author hp
+ */
+@Getter
+@Setter
+public class PageResult<T> {
+    private Long total;
+    private Integer pageSize;
+    private Integer pageNumber;
+    private List<T> list;
+
+    public PageResult() {
+    }
+
+    public PageResult(List<T> list, Long total, Integer pageSize, Integer pageNumber) {
+        this.list = list;
+        this.total = total;
+        this.pageSize = pageSize;
+        this.pageNumber = pageNumber;
+    }
+
+    public static <T> PageResult of(List<T> list, Long total, Integer pageSize, Integer pageNumber) {
+        return new PageResult(list, total, pageSize, pageNumber);
+    }
+}
