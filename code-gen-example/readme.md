b5065996f711190f5b1da7e40dc67be70d05cd75
hupeng <hup_dev@outlook.com>
2023-07-25 11:54:50 +0800

    code-gen demo
    Signed-off-by: hupeng <hup_dev@outlook.com>

--- code-gen-example/readme.md
+++ code-gen-example/readme.md
@@ -1,5 +1,17 @@
+Usage
+
 1. mvn clean
 2. mvn install -pl com.luban:code-gen-common-spring-boot-starter -am -Dtest.skip=true
 3. mvn compile -pl com.luban:test-code-gen-module -am -Dtest.skip=true
+
+Note
+
+By default, this module generates codes with Spring Data Jpa.
+
+To generate codes that adhere to the Mybatis-plus framework, passing the argument `-Aorm=mbp` when calling the compilation process.
+
+1. mvn compile -pl com.luban:test-code-gen-module -am -Dtest.skip=true -Aorm=mbp 
+
+Debug
 
 - debug通过idea等编译器的maven插件运行时的debug模式来做调试
--- code-gen-example/src/main/java/com/luban/codegen/test/domain/TestOrder.java
+++ code-gen-example/src/main/java/com/luban/codegen/test/domain/TestOrder.java
@@ -1,5 +1,7 @@
 package com.luban.codegen.test.domain;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
 import com.luban.codegen.processor.controller.GenController;
 import com.luban.codegen.processor.dto.GenDto;
 import com.luban.codegen.processor.mapper.GenMapper;
@@ -11,36 +13,36 @@ import com.luban.codegen.processor.service.GenServiceImpl;
 import com.luban.codegen.processor.vo.GenVo;
 import com.luban.common.base.annotations.FieldDesc;
 import com.luban.common.base.enums.ValidStatus;
-import com.luban.jpa.BaseJpaAggregate;
 import com.luban.jpa.convertor.LocalDateTimeConverter;
-import com.luban.jpa.convertor.ValidStatusConverter;
+import com.luban.mybatisplus.BaseMbpAggregate;
+import com.luban.mybatisplus.convertor.LocalDateTimeTypeConverter;
+import com.luban.mybatisplus.convertor.ValidStatusConverter;
 import lombok.Data;
 
 import javax.persistence.Convert;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import java.io.Serializable;
 import java.time.LocalDateTime;
 
 /**
- * @author HP
- * @date 2022/10/24
+ * @author hp 2023/4/10
  */
-@GenResponse(pkgName = "com.luban.codegen.test.domain.response")
-@GenVo(pkgName = "com.luban.codegen.test.domain.response")
-@GenDto(pkgName = "com.luban.codegen.test.domain.request")
 @GenRequest(pkgName = "com.luban.codegen.test.domain.request")
-@GenController(pkgName = "com.luban.codegen.test.domain.controller")
+@GenResponse(pkgName = "com.luban.codegen.test.domain.response")
+@GenDto(pkgName = "com.luban.codegen.test.domain.request")
+@GenVo(pkgName = "com.luban.codegen.test.domain.response")
+@GenController(pkgName = "com.luban.codegen.test.controller")
 @GenService(pkgName = "com.luban.codegen.test.domain.service")
 @GenServiceImpl(pkgName = "com.luban.codegen.test.domain.service.impl")
 @GenRepository(pkgName = "com.luban.codegen.test.domain.repository")
 @GenMapper(pkgName = "com.luban.codegen.test.domain.mapper")
-@Entity
-@Table
+@TableName(value="test_order")
 @Data
-public class TestOrder extends BaseJpaAggregate implements Serializable {
+public class TestOrder extends BaseMbpAggregate {
 
-    @Convert(converter = ValidStatusConverter.class)
+    @TableField(typeHandler = ValidStatusConverter.class)
+    @FieldDesc("状态")
+    private ValidStatus status;
+
+    @Convert(converter = com.luban.jpa.convertor.ValidStatusConverter.class)
     @FieldDesc("状态")
     private ValidStatus validStatus;
 
@@ -48,18 +50,22 @@ public class TestOrder extends BaseJpaAggregate implements Serializable {
     @FieldDesc("没有实际converter")
     private ValidStatus noConverter;
 
+    @TableField(typeHandler = LocalDateTimeTypeConverter.class)
     @Convert(converter = LocalDateTimeConverter.class)
     @FieldDesc("时间")
     private LocalDateTime date;
 
     private Long testLong;
 
-    public void init() {
+    public void init(){
+        setStatus(ValidStatus.VALID);
     }
 
-    public void valid() {
+    public void valid(){
+        setStatus(ValidStatus.VALID);
     }
 
-    public void invalid() {
+    public void invalid(){
+        setStatus(ValidStatus.INVALID);
     }
 }
