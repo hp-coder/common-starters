da4b3c4284484e49e3197f6a43a78ed63d1adca5
hupeng <hup_dev@outlook.com>
2023-10-13 15:38:36 +0800

    Documentation
    Signed-off-by: hupeng <hup_dev@outlook.com>

--- join-in-memory-common-spring-boot-starter/src/main/java/com/luban/joininmemory/annotation/JoinInMemory.java
+++ join-in-memory-common-spring-boot-starter/src/main/java/com/luban/joininmemory/annotation/JoinInMemory.java
@@ -12,44 +12,87 @@ import java.lang.annotation.Target;
 @Target({ElementType.FIELD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface JoinInMemory {
+
     /**
-     * 从 sourceData 中提取 key
-     * 如：查询订单列表时，关联用户数据的key为userId
+     * Using SpEL to extract the field value from the given source.
+     * <p>
+     * Usage Example:
+     * <p>
+     * "#{user.id}"
+     *
+     * @return SpEL expression of the key extracted from the given source
      */
     String keyFromSourceData();
 
     /**
-     * 转换数据源的key类型
+     * Convert the source field value to a new value according
+     * to the SpEL expression given.
+     * Won't convert if it is empty.
+     * <p>
+     * In some cases, the type of the field in the entity
+     * is different from the type of the field defined in
+     * the source object.
+     * <p>
+     * Usage Example:
+     * <p>
+     * "#{ #root != null ? T(java.lang.Long).parseLong(#root) : null }"
+     *
+     * @return SpEL expression of the converted source field value. This value is used in
+     * the mapping process.
      */
     String sourceDataKeyConverter() default "";
 
     /**
-     * 从 joinData 中提取 key
-     * 如：查询到用户所有订单后，通过userId，查询到关联的用户信息，用户信息中与订单关联的信息为其id
+     * Using SpEL to extract field value from the datasource.
+     * Define using which field to construct a mapping relation.
      * <p>
-     * 用于构造Map映射关系的key值
+     * Usage Example:
+     * <p>
+     * "#{userId}"
+     *
+     * @return SpEL expression of the field value retrieved from the datasource
      */
     String keyFromJoinData();
 
     /**
-     * 转换数据源的key类型
+     * Same as the sourceDataKeyConverter()
+     * Won't convert if it is empty.
+     *
+     * @return SpEL expression of the converted field value extracted from the datasource
      */
     String joinDataKeyConverter() default "";
 
     /**
-     * 批量数据抓取
-     * 如：查询订单列表，订单通过userId关联用户信息，使用什么方法通过userId获取到用户信息
+     * Define a method which takes one parameter contains all the values composed of keyFromSourceData()
+     * and returns a list of values from the datasource.
+     * Eventually, the list will be converted into a joinDataKeyConverter keyed map.(Map<KEY,List<CONVERTED_OBJECT>>)
+     * <p>
+     * Usage example:
+     * <p>
+     * "#{@userRepository.findAllById(#root)}"
+     *
+     * @return SpEL expression of the method that extracts the data from datasource
      */
     String loader();
 
     /**
-     * 结果转换器
-     * 如：查询到关联的用户信息时通过ORM映射返回的用户对象，在业务中常需要转换为VO及其他对象，如何转换由这里定义
+     * Define a method that takes one parameter,which is the object from the data source,
+     * and returns the converted result which is the type of the field annotated with the join annotation.
+     * <p>
+     * In the end, if data source values can be found through the mapping process,
+     * the annotated source field will be set in two ways.
+     * If the annotated source field is a List type, it will be set accordingly.
+     * Or, if the field is the List's parameter type, it only takes one object as its value.
+     * So the field will be set only if the converted list has only one element.
+     * Otherwise, the list is discarded.
+     * <p>
+     * Usage example:
+     * <p>
+     * "#{#root.name}"
+     *
+     * @return SpEL expression of how to converter values from datasource if match the key value from the keyFromSourceData()
      */
     String joinDataConverter() default "";
 
-    /**
-     * 运行级别，同一级别的 join 可 并行执行
-     */
     int runLevel() default 10;
 }
--- join-in-memory-common-spring-boot-starter/src/main/java/com/luban/joininmemory/annotation/JoinInMemoryConfig.java
+++ join-in-memory-common-spring-boot-starter/src/main/java/com/luban/joininmemory/annotation/JoinInMemoryConfig.java
@@ -1,5 +1,7 @@
 package com.luban.joininmemory.annotation;
 
+import com.luban.joininmemory.JoinInMemoryAutoConfiguration;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -12,7 +14,35 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface JoinInMemoryConfig {
+
+    /**
+     * Defines how to execute tasks.
+     * <p>
+     * Note:
+     * <p>
+     * Using the default thread pool,
+     * which was using a SynchronousQueue to store tasks,
+     * will fail at nested join tasks.
+     * When defining nested join is unavoidable, use SERIAL.
+     * This specific scenario usually occurs when @AfterJoin is used.
+     * <p>
+     * Due to executing tasks with different thread pools,
+     * mix-using both executor types is unsafe and will throw a runtime exception.
+     *
+     * @return way to execute tasks
+     * @see AfterJoin
+     * @see JoinInMemoryAutoConfiguration
+     */
     JoinInMemoryExecutorType executorType() default JoinInMemoryExecutorType.SERIAL;
 
+    /**
+     * ExecutorService used to execute tasks
+     * <p>
+     * Note:
+     * <p>
+     * Make sure the class is already correctly registered as a Spring Bean.
+     *
+     * @return the bean name of the defined executor service.
+     */
     String executorName() default "defaultJoinInMemoryExecutor";
 }
