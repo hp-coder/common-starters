5584251c0fa2590df74032bc807483b39449956c
hupeng <hup_dev@outlook.com>
2023-10-25 10:32:05 +0800

    Test the optimized starter.
    Signed-off-by: hupeng <hup_dev@outlook.com>

--- excel-starter-example/src/main/java/com/luban/excel/example/select_data_handler/DynamicSelectPrimaryHandler.java
+++ excel-starter-example/src/main/java/com/luban/excel/example/select_data_handler/DynamicSelectPrimaryHandler.java
@@ -16,6 +16,10 @@ public class DynamicSelectPrimaryHandler implements ColumnDynamicSelectDataHandl
     @Override
     public Function<String, List<String>> source() {
         //这里就做数据的查询，最后返回一个List<String>即可， 对于该方法的入参param没设计好，暂时影响不大
-        return param -> Arrays.asList("级联第一级1","级联第一级2","级联第一级3");
+        return param -> Arrays.asList(
+                "级联第一级1",
+                "级联第一级2",
+                "级联第一级3"
+        );
     }
 }
