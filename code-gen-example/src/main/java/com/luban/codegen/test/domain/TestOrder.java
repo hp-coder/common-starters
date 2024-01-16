30676a7c9aa04287c0ad379aa21e890a1dfb8c5c
hupeng <hup_dev@outlook.com>
2024-01-16 12:00:07 +0800

    Code gen example
    Signed-off-by: hupeng <hup_dev@outlook.com>

--- code-gen-example/src/main/java/com/luban/codegen/test/domain/TestOrder.java
+++ code-gen-example/src/main/java/com/luban/codegen/test/domain/TestOrder.java
@@ -1,10 +1,27 @@
 package com.luban.codegen.test.domain;
 
+import com.luban.codegen.processor.api.GenFeign;
+import com.luban.codegen.processor.command.create.GenCreateCommand;
+import com.luban.codegen.processor.command.update.GenUpdateCommand;
+import com.luban.codegen.processor.context.create.GenCreateContext;
+import com.luban.codegen.processor.context.update.GenUpdateContext;
+import com.luban.codegen.processor.controller.GenController;
+import com.luban.codegen.processor.dto.GenDto;
+import com.luban.codegen.processor.event.GenEvent;
+import com.luban.codegen.processor.event.GenEventListener;
 import com.luban.codegen.processor.mapper.GenMapper;
 import com.luban.codegen.processor.repository.GenRepository;
+import com.luban.codegen.processor.request.GenCreateRequest;
+import com.luban.codegen.processor.request.GenPageRequest;
 import com.luban.codegen.processor.request.GenRequest;
+import com.luban.codegen.processor.request.GenUpdateRequest;
+import com.luban.codegen.processor.response.GenPageResponse;
+import com.luban.codegen.processor.response.GenResponse;
 import com.luban.codegen.processor.service.GenService;
 import com.luban.codegen.processor.service.GenServiceImpl;
+import com.luban.codegen.processor.vo.GenVo;
+import com.luban.codegen.test.domain.context.CreateTestOrderContext;
+import com.luban.codegen.test.domain.context.UpdateTestOrderContext;
 import com.luban.common.base.annotations.FieldDesc;
 import com.luban.common.base.enums.ValidStatus;
 import com.luban.jpa.BaseJpaAggregate;
@@ -12,7 +29,8 @@ import com.luban.jpa.convertor.ValidStatusConverter;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.proxy.HibernateProxy;
 
 import java.util.Objects;
@@ -21,9 +39,22 @@ import java.util.Optional;
 /**
  * @author hp
  */
-
-
-@GenRequest(pkgName =  "com.luban.codegen.test.domain.request")
+@GenFeign(pkgName = "com.luban.codegen.test.api", serverName = "orderServer")
+@GenRequest(pkgName = "com.luban.codegen.test.domain.request")
+@GenCreateRequest(pkgName = "com.luban.codegen.test.domain.request")
+@GenUpdateRequest(pkgName = "com.luban.codegen.test.domain.request")
+@GenPageRequest(pkgName = "com.luban.codegen.test.domain.request")
+@GenResponse(pkgName = "com.luban.codegen.test.domain.response")
+@GenPageResponse(pkgName = "com.luban.codegen.test.domain.response")
+@GenEvent(pkgName = "com.luban.codegen.test.domain.events")
+@GenVo(pkgName = "com.luban.codegen.test.domain.request")
+@GenDto(pkgName = "com.luban.codegen.test.domain.response")
+@GenEventListener(pkgName = "com.luban.codegen.test.domain.events")
+@GenCreateContext(pkgName = "com.luban.codegen.test.domain.context")
+@GenUpdateContext(pkgName = "com.luban.codegen.test.domain.context")
+@GenCreateCommand(pkgName = "com.luban.codegen.test.domain.command")
+@GenUpdateCommand(pkgName = "com.luban.codegen.test.domain.command")
+@GenController(pkgName = "com.luban.codegen.test.controller")
 @GenService(pkgName = "com.luban.codegen.test.domain.service")
 @GenServiceImpl(pkgName = "com.luban.codegen.test.domain.service.impl")
 @GenRepository(pkgName = "com.luban.codegen.test.domain.repository")
@@ -32,23 +63,21 @@ import java.util.Optional;
 @Table(name = "test_order")
 @Getter
 @Setter
-@ToString
-@RequiredArgsConstructor
 public class TestOrder extends BaseJpaAggregate {
 
     @Convert(converter = ValidStatusConverter.class)
     @FieldDesc("状态")
     private ValidStatus status;
 
-//    public static TestOrder createTestOrder(CreateTestOrderContext context) {
-//        final TestOrder entity = new TestOrder();
-//        context.setEntity(entity);
-//        return entity;
-//    }
-//
-//    public void updateTestOrder(UpdateTestOrderContext context) {
-//        context.setEntity(this);
-//    }
+    public static TestOrder createTestOrder(CreateTestOrderContext context){
+        final TestOrder entity = new TestOrder();
+        context.setEntity(entity);
+        return entity;
+    }
+
+    public void updateTestOrder(UpdateTestOrderContext context){
+        context.setEntity(this);
+    }
 
     public boolean enabled() {
         return Optional.ofNullable(getStatus())
@@ -60,27 +89,33 @@ public class TestOrder extends BaseJpaAggregate {
         return !enabled();
     }
 
-    public void init() {
+    public void init(){
         setStatus(ValidStatus.VALID);
     }
 
-    public void valid() {
+    public void valid(){
         setStatus(ValidStatus.VALID);
     }
 
-    public void invalid() {
+    public void invalid(){
         setStatus(ValidStatus.INVALID);
     }
 
     @Override
     public final boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
         Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
         Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
-        if (thisEffectiveClass != oEffectiveClass) return false;
-        TestOrder testOrder = (TestOrder) o;
-        return getId() != null && Objects.equals(getId(), testOrder.getId());
+        if (thisEffectiveClass != oEffectiveClass) {
+            return false;
+        }
+        TestOrder that = (TestOrder) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
     }
 
     @Override
