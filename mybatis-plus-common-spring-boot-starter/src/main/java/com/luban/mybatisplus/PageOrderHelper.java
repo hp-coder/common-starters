b948bbc9e0b42d7606224c231e648d2716c41177
hupeng <hup_dev@outlook.com>
2024-02-29 10:34:27 +0800

    Mybatis module adjustments
    1. Generic type
    
    Signed-off-by: hupeng <hup_dev@outlook.com>

--- mybatis-plus-common-spring-boot-starter/src/main/java/com/luban/mybatisplus/PageOrderHelper.java
+++ mybatis-plus-common-spring-boot-starter/src/main/java/com/luban/mybatisplus/PageOrderHelper.java
@@ -2,6 +2,7 @@ package com.luban.mybatisplus;
 
 import com.baomidou.mybatisplus.core.metadata.OrderItem;
 import com.luban.common.base.model.PageRequestWrapper;
+import com.luban.common.base.model.Request;
 import org.springframework.util.CollectionUtils;
 
 import java.util.Collections;
@@ -14,7 +15,7 @@ import java.util.stream.Collectors;
  */
 public final class PageOrderHelper {
 
-    public static <T> List<OrderItem> orders(PageRequestWrapper<T> wrapper) {
+    public static <T extends Request> List<OrderItem> orders(PageRequestWrapper<T> wrapper) {
         final List<PageRequestWrapper.OrderColumn> sorts = wrapper.getSorts();
         if (CollectionUtils.isEmpty(sorts)) {
             return Collections.emptyList();
